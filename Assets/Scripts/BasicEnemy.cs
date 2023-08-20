dd231c9368dc5d1d763e1c9c783eba7d0a30d07f
Reid3002 <reid3002@gmail.com>
2023-08-20 17:02:26 -0300

    Main Commit

---
+++ Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (target.transform.position.x != this.transform.position.x)
+        {
+           rb.velocity = new Vector2 (target.transform.position.x - transform.position.x, transform.position.y);
+        }
+       else if (target.transform.position.y != this.transform.position.y)
+        {
+            rb.velocity = new Vector2(transform.position.x, target.transform.position.y - transform.position.y);
+        }
+    }
+}
