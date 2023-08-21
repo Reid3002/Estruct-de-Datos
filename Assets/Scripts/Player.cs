d0f3117e84972a02a0557f11c62c5c2b3c199b91
Reid3002 <reid3002@gmail.com>
2023-08-21 17:49:36 -0300

    Player vertical movement fix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
     [SerializeField] int moveSpeed = 5;
+    private Queue<GameObject> greenDots = new Queue<GameObject>();
 
     public float direction;
     // Start is called before the first frame update
@@ -37,6 +38,15 @@ public class Player : MonoBehaviour
         else if (Input.GetAxis("Horizontal") < 0)
         {
             transform.eulerAngles = new Vector3 (0, 180, 0);
+        }
+
+        if (Input.GetAxis ("Vertical") > 0)
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
+        else if (Input.GetAxis ("Vertical") < 0)
+        {
+            transform.eulerAngles = new Vector3(180, 0, 0);
         }
  
     }
