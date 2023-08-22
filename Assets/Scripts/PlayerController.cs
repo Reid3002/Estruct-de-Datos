49adad1568502b820595057d7b50f48c0def0828
Arielga <81482233+a7xpadd3r@users.noreply.github.com>
2023-08-22 20:48:36 -0300

    first

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public Snake theSnake;
 
     [Header ("Movement")]
@@ -22,11 +23,32 @@ public class PlayerController : MonoBehaviour
         this.theSnake = new Snake(this.transform.position, Snake.Direction.None);
     }
 
+=======
+    public enum Direction { None, Up, Down, Left, Right }
+
+    [Header("Settings")]
+    [Range(0.005f, 0.5f)] public float stepAmount = 0.05f;
+    [Range(0.001f, 1)] public float stepTime = 0.5f;
+    private float currentStepTime = 0;
+
+    [Header("Direction")]
+    public Direction lastSuccessfulDirection = Direction.None;
+    public Direction desiredDirection = Direction.None;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.currentStepTime = this.stepTime;
+    }
+
+    // Update is called once per frame
+>>>>>>> Stashed changes
     void Update()
     {
         float hAxis = Input.GetAxisRaw("Horizontal");
         float vAxis = Input.GetAxisRaw("Vertical");
 
+<<<<<<< Updated upstream
         if (hAxis != 0 || vAxis != 0)
         {
             if (hAxis < 0 && this.theSnake.lastDirection != Snake.Direction.Right)          
@@ -116,6 +138,57 @@ public class PlayerController : MonoBehaviour
     {
         float snapX = Mathf.Round(input.x / stepSize) * stepSize;
         float snapY = Mathf.Round(input.y / stepSize) * stepSize;
+=======
+        if (hAxis != 0)
+        {
+            if (hAxis > 0) this.desiredDirection= Direction.Right;
+            else this.desiredDirection= Direction.Left;
+        }
+
+        if (vAxis != 0) 
+        {
+            if (vAxis > 0) this.desiredDirection = Direction.Up;
+            else this.desiredDirection= Direction.Down;
+        }
+
+        if (this.currentStepTime > 0)
+            this.currentStepTime -= Time.deltaTime;
+        else
+        {
+            if ((this.desiredDirection == Direction.Up || this.desiredDirection == Direction.Down) && IsRoundNumber(this.transform.position.x))
+                this.lastSuccessfulDirection = this.desiredDirection;
+            else if ((this.desiredDirection == Direction.Left|| this.desiredDirection == Direction.Right) && IsRoundNumber(this.transform.position.y))
+                this.lastSuccessfulDirection = this.desiredDirection;
+
+            Vector3 newDirection = Vector3.zero;
+
+            switch (this.lastSuccessfulDirection)
+            {
+                case Direction.Up:
+                    newDirection = Vector3.up;
+                    break;
+                case Direction.Down:
+                    newDirection = Vector3.down;
+                    break;
+                case Direction.Left:
+                    newDirection = Vector3.left;
+                    break;
+                case Direction.Right:
+                    newDirection = Vector3.right;
+                    break;
+            }
+
+            this.transform.position += newDirection * stepAmount;
+            this.transform.position = ForceRoundPosition(this.transform.position);
+            this.currentStepTime = this.stepTime;
+        }
+    }
+
+    Vector3 ForceRoundPosition(Vector3 input)
+    {
+        float snapX = Mathf.Round(input.x / stepAmount) * stepAmount;
+        float snapY = Mathf.Round(input.y / stepAmount) * stepAmount;
+>>>>>>> Stashed changes
 
         return new Vector3(snapX, snapY);
     }
@@ -125,7 +198,20 @@ public class PlayerController : MonoBehaviour
         int roundedInt = Mathf.RoundToInt(number);
         float difference = Mathf.Abs(number - roundedInt);
 
+<<<<<<< Updated upstream
         return difference < tolerance;
     }
 
+=======
+        if (difference < tolerance)
+        {
+            return true;
+        }
+        else
+        {
+            float fractionalPart = number - Mathf.Floor(number);
+            return Mathf.Abs(fractionalPart - 0.5f) < tolerance;
+        }
+    }
+>>>>>>> Stashed changes
 }
