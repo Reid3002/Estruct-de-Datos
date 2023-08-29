64a7caa302f314b2dde6ab400df68c6a83a1befa
Arielga <81482233+a7xpadd3r@users.noreply.github.com>
2023-08-29 20:19:34 -0300

    Added steps amount

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,9 +8,11 @@ public class PlayerController : SnakeController
     public SnakeController testTail;
 
     [Header("Settings")]
-    [Range(0.005f, 0.5f)] public float stepAmount = 0.05f;
-    [Range(0.00001f, 1)] public float stepTime = 0.5f;
     [Range(0.05f, 5)] public float newTileSize = 0.5f;
+    [Range(0.005f, 0.5f)] public float stepAmount = 0.05f;
+    [Range(0.0001f, 1)] public float stepTime = 0.5f;
+    [Range(1, 10)] public int steps = 1;
+
 
     private float currentStepTime = 0;
 
@@ -43,40 +45,44 @@ public class PlayerController : SnakeController
             this.currentStepTime -= Time.deltaTime;
         else
         {
-            // Call update tail
-            if (this.tailObject != null)
-                this.tailObject.MoveTail(this.stepAmount, this.lastSuccessfulDirection);
-
-            // Check for direction change ( X - Y ) - snake like movement
-            if ((this.desiredDirection == Direction.Up || this.desiredDirection == Direction.Down) && (this.lastSuccessfulDirection != Direction.Up && this.lastSuccessfulDirection != Direction.Down) && IsRoundNumber(this.transform.position.x))
-                UpdateDirectionData();
-
-            else if ((this.desiredDirection == Direction.Left || this.desiredDirection == Direction.Right) && (this.lastSuccessfulDirection != Direction.Left && this.lastSuccessfulDirection != Direction.Right) && IsRoundNumber(this.transform.position.y))
-                UpdateDirectionData();
-
-            // Generate vector and give it the direction
-            Vector3 newDirection = Vector3.zero;
-
-            switch (this.lastSuccessfulDirection)
+            // Make X steps
+            for (int i = 0; i < steps; i++)
             {
-                case Direction.Up:
-                    newDirection = Vector3.up;
-                    break;
-                case Direction.Down:
-                    newDirection = Vector3.down;
-                    break;
-                case Direction.Left:
-                    newDirection = Vector3.left;
-                    break;
-                case Direction.Right:
-                    newDirection = Vector3.right;
-                    break;
-            }
+                // Call update tail
+                if (this.tailObject != null)
+                    this.tailObject.MoveTail(this.stepAmount, this.lastSuccessfulDirection);
 
-            // Change object position & time
-            this.transform.position += newDirection * this.stepAmount;
-            this.transform.position = ForceRoundPosition(this.transform.position, stepAmount);
-            this.currentStepTime = this.stepTime;
+                // Check for direction change ( X - Y ) - snake like movement
+                if ((this.desiredDirection == Direction.Up || this.desiredDirection == Direction.Down) && (this.lastSuccessfulDirection != Direction.Up && this.lastSuccessfulDirection != Direction.Down) && IsRoundNumber(this.transform.position.x))
+                    UpdateDirectionData();
+
+                else if ((this.desiredDirection == Direction.Left || this.desiredDirection == Direction.Right) && (this.lastSuccessfulDirection != Direction.Left && this.lastSuccessfulDirection != Direction.Right) && IsRoundNumber(this.transform.position.y))
+                    UpdateDirectionData();
+
+                // Generate vector and give it the direction
+                Vector3 newDirection = Vector3.zero;
+
+                switch (this.lastSuccessfulDirection)
+                {
+                    case Direction.Up:
+                        newDirection = Vector3.up;
+                        break;
+                    case Direction.Down:
+                        newDirection = Vector3.down;
+                        break;
+                    case Direction.Left:
+                        newDirection = Vector3.left;
+                        break;
+                    case Direction.Right:
+                        newDirection = Vector3.right;
+                        break;
+                }
+
+                // Change object position & time
+                this.transform.position += newDirection * this.stepAmount;
+                this.transform.position = ForceRoundPosition(this.transform.position, stepAmount);
+                this.currentStepTime = this.stepTime;
+            }
         }
 
         // Test - Add tail
