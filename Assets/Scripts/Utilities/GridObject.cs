1dec9865fd4fac1d2295e2b047aee40856e4e34f
Arielga <81482233+a7xpadd3r@users.noreply.github.com>
2023-11-01 16:26:24 -0300

    Grid testing (scene Test01)

---
+++ Assets/Scripts/Utilities/GridObject.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridObject : MonoBehaviour
+{
+    public bool isNavigable = true;
+    public bool hasPlayer = false;
+    public NavigationNodeTestScript node;
+
+    public string testString = "";
+}
