1b071f5bac7d31be7ab6821a61cbe8fc7ce81603
Reid3002 <reid3002@gmail.com>
2023-11-10 01:34:17 -0300

    Minor fixes

---
+++ Assets/Scripts/Utilities/IGraph.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IGraph
+{
+    void InicializarGrafo();
+    void AgregarVertice(int v);
+    void EliminarVertice(int v);
+    ConjuntoTDA Vertices();
+    void AgregarArista(int v1, int v2, int peso);
+    void EliminarArista(int v1, int v2);
+    bool ExisteArista(int v1, int v2);
+    int PesoArista(int v1, int v2);
+}
